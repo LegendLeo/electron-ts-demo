1fb1bec8fa25dada2a7ce368ecf59e92f58daa6a
Leo <hqyx45@qq.com>
2020-01-16 00:13:19 +0800

    :zap: 优化工具方法

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,28 +7,6 @@ export function $(str: string): HTMLElement {
   return document.getElementById(str)
 }
 
-// 因为BrowserWindow不支持继承，封装类的方法被弃用
-// export class AppWindow extends BrowserWindow {
-//   constructor(
-//     templateLocation: string,
-//     config?: Electron.BrowserWindowConstructorOptions
-//   ) {
-//     const baseConfig = {
-//       width: 800,
-//       height: 600,
-//       webPreferences: {
-//         nodeIntegration: true
-//       }
-//     }
-//     const finalConfig = { ...baseConfig, ...config }
-//     super(finalConfig)
-//     this.loadURL('file://' + templateLocation)
-//     this.once('ready-to-show', () => {
-//       this.show()
-//     })
-//   }
-// }
-
 /**
  * 创建窗口
  * @param templateLocation 模板位置
@@ -52,7 +30,6 @@ export function createWindow(
   })
   return window
 }
-
 
 export class DataStore extends Store {
   constructor(options: object) {
